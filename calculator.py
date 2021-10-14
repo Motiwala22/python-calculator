795d15a9f7158798711921ea15144ca8370726d9
Gurudatta Kharatmal <92509537+GurudattaTheDev@users.noreply.github.com>
2021-10-14 19:47:01 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,31 @@
+
+print("1: Addition")
+print("2: Subtraction")
+print("3: Multiplication")
+print("4: Division")
+
+choice = input("Enter your chocie here : ")
+
+num1 = float(input())
+num2 = float(input())
+
+if choice == "1":
+    print( num1, "+", num2, "=", (num1+num2))
+
+elif choice == "2":
+    print( num1, "-", num2, "=", (num1-num2))
+
+elif choice == "3":
+    print( num1, "*", num2, "=", (num1*num2))
+
+elif choice == "4":
+    if num2 == 0.0:
+        print("Divided by 0; Error 404")
+    else:
+        print( num1, "/", num2, "=", (num1/num2))
+
+else:
+    print("Invaild choice")
+
+
+     
